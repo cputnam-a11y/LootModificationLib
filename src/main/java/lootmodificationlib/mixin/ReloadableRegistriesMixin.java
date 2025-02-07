b6fd2a1a5b3213d3207194aa8e27524c14440364
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-02-07 17:40:38 -0600

    Backport to 1.21.1

--- src/main/java/lootmodificationlib/mixin/ReloadableRegistriesMixin.java
+++ src/main/java/lootmodificationlib/mixin/ReloadableRegistriesMixin.java
@@ -16,7 +16,7 @@ public class ReloadableRegistriesMixin {
     // javac just doesn't like it because LootDataType is a record and thus final
     // the unchecked is safe because the consumer that takes G is set on the LootDataType, which is also of type G
     @SuppressWarnings({"DataFlowIssue", "unchecked"})
-    @Inject(method = "method_61246", at = @At("HEAD"))
+    @Inject(method = "method_58286", at = @At("HEAD"))
     private static <G> void onBeforeAddToRegistry(MutableRegistry<G> mutableRegistry, LootDataType<G> rawLootDataType, Identifier id, Object /*G*/ value, CallbackInfo ci) {
         var lootDataType = (LootDataType<G> & LootDataTypeExtension<G>) (Object) rawLootDataType;
         lootDataType.lootmodificationlib$getIdSetter().accept((G) value, id);
